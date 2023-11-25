1ce7e723a56ec6b87ba94debcdc244598430a8ea
Hipolito Colela <laboralespolo@hotmail.com>
2023-11-25 20:25:01 -0300

    desafioEntregable3Colela

--- productManager.js
+++ productManager.js
@@ -4,7 +4,8 @@ class ProductManager{
 	constructor(){
 		this.nextProductId = 1;
 	};
-  productos = [];
+    
+    productos = [];
   
 	//create
 	async addProduct(title, description, price, thumbnail, code, stock){
@@ -20,6 +21,7 @@ class ProductManager{
 		return productosArchivados;
 	}
 
+    //read limit
     async getLimitedProducts(limit){
         let productosArchivados = await fs.promises.readFile('productos.json', 'utf-8');
         productosArchivados = JSON.parse(productosArchivados);
@@ -30,12 +32,15 @@ class ProductManager{
             return productosArchivados.slice(0, parseInt(limit));
         }
     }
+
 	//readById
 	async getProductById(id) {
         let productosArchivados = await this.getProducts();
         const product = productosArchivados.find(producto => producto.id === id);
         return product;
     }
+
+    //update
 	async updateProduct(id, updatedData) {
         let productosArchivados = await this.getProducts();
         const index = productosArchivados.findIndex(producto => producto.id === id);
@@ -48,6 +53,8 @@ class ProductManager{
         }
         return false; // Indica que no se encontró el producto con el ID especificado
     }
+
+    //delete
 	async deleteProduct(id) {
         let productosArchivados = await this.getProducts();
         const updatedProducts = productosArchivados.filter(producto => producto.id !== id);
@@ -62,18 +69,25 @@ class ProductManager{
 }
 
 async function productosAsincronos(){
+    // Crear una instancia del ProductManager
 	const managerProduct = new ProductManager();
-	await managerProduct.addProduct('procesador', 'next generation', 190, 'sin imagen', 'abc123', 5);
+	// Añadir algunos productos de ejemplo
+    await managerProduct.addProduct('procesador', 'next generation', 190, 'sin imagen', 'abc123', 5);
 	await managerProduct.addProduct('tarjeta de video', 'high performance', 300, 'otra imagen', 'def456', 10);
 	await managerProduct.addProduct('mother', 'ultra power', 210, 'otra imagen más', 'ghi789', 8);
 	await managerProduct.addProduct('memoria ram', 'top memory', 25, 'nueva imagen', 'jkl 012', 3);
     await managerProduct.addProduct('estabilizador celular', 'smooth', 75, 'otra imagen nueva', 'mno345', 1);
+    await managerProduct.addProduct('camera', 'free bitrate', 210, 'otra vez sin imagen','pqr678', 11);
+    
+    // Imprimir la lista de productos
     console.log (await managerProduct.getProducts());
 
+    // Obtener un producto por su ID (en este caso, el ID es 2)
 	const productIdToGet = 2; 
     const productById = await managerProduct.getProductById(productIdToGet);
     console.log(`Producto con ID ${productIdToGet}:`, productById);
 
+    // Actualizar un producto por su ID (en este caso, el ID es 1)
 	const productIdToUpdate = 1;
     const updatedData = {
         title: 'Nuevo Procesador',
@@ -83,26 +97,37 @@ async function productosAsincronos(){
 
     const isUpdated = await managerProduct.updateProduct(productIdToUpdate, updatedData);
     
+    // Imprimir un mensaje indicando si la actualización fue exitosa o no
     if (isUpdated) {
         console.log(`Producto con ID ${productIdToUpdate} actualizado.`);
     } else {
         console.log(`No se encontró un producto con ID ${productIdToUpdate}.`);
     }
+
+    // Imprimir la lista de productos después de la actualización
     console.log(await managerProduct.getProducts());
 
+    // Eliminar un producto por su ID (en este caso, el ID es 1)
 	const productIdToDelete = 1;
 
     const isDeleted = await managerProduct.deleteProduct(productIdToDelete);
     
+    // Imprimir un mensaje indicando si la eliminación fue exitosa o no
     if (isDeleted) {
         console.log(`Producto con ID ${productIdToDelete} eliminado.`);
     } else {
         console.log(`No se encontró un producto con ID ${productIdToDelete}.`);
     }
 
+    // Imprimir la lista de productos después de la eliminación
     console.log(await managerProduct.getProducts());
 }
 
-productosAsincronos();
+// Si este script se está ejecutando directamente, llamar a la función productosAsincronos
+if (require.main === module) {
+    // Esta condición verifica si el script se está ejecutando directamente
+    productosAsincronos();
+}
 
+// Exportar la clase ProductManager (para ser utilizada en otros archivos si es necesario)
 module.exports = ProductManager;
